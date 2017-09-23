12ab512656bbe012c4f0726eb9206c5759bb35bc
Guyu Fan <guyufan1996@gmail.com>
2017-09-23 11:51:03 -0400

    change header file to long long

--- stoplights/solve.h
+++ stoplights/solve.h
@@ -7,6 +7,6 @@
 #include <string>
 #include <vector>
 
-std::string solve(std::vector<std::vector<graph_edge> > graph, int startIndex, int endIndex);
+std::string solve(std::vector<std::vector<graph_edge> > graph, long long startIndex, long long endIndex);
 
 #endif
