86e6ea10aff8bc78b4a76aacd69fab2e363959cf
Guyu Fan <guyufan1996@gmail.com>
2017-09-23 11:50:21 -0400

    long live the long long, and stoplightDist returns -1 for uncrossable edge

--- stoplights/solve.cpp
+++ stoplights/solve.cpp
@@ -9,10 +9,10 @@
 using namespace std;
 
 struct Node {
-  int index; // index of node
+  long long index; // index of node
   long long dist; // distance of node from source
-  int arrivedAt; // clock time of arrival (-1 for nodes not yet reached)
-  Node(int i, long long d, int t) {
+  long long arrivedAt; // clock time of arrival (-1 for nodes not yet reached)
+  Node(long long i, long long d, long long t) {
     index = i;
     dist = d;
     arrivedAt = t;
@@ -23,11 +23,11 @@ struct Node {
 auto comp = [] (Node &a, Node &b) -> bool { return a.dist > b.dist; };
 
 // get the stopLight distance (accounting for waiting time)
-int getStoplightDist(int clockTime, graph_edge edge) {
+long long getStoplightDist(long long clockTime, graph_edge edge) {
   if (edge.redtime == 0) return edge.traversetime;
-  if (edge.greentime < edge.traversetime) return numeric_limits<long long>::max();
-  int cycle = edge.greentime + edge.redtime;
-  int stoplightStatus = edge.greentime - (clockTime % cycle);
+  if (edge.greentime < edge.traversetime) return -1;
+  long long cycle = ((long long) edge.greentime) + edge.redtime;
+  long long stoplightStatus = edge.greentime - (clockTime % cycle);
   // edge is currently red or not green enough to be traversed
   if (stoplightStatus < edge.traversetime) {
     return edge.traversetime + (cycle - (clockTime % cycle));
@@ -36,17 +36,17 @@ int getStoplightDist(int clockTime, graph_edge edge) {
   return edge.traversetime;
 }
 
-string solve(vector<vector<graph_edge> > graph, int startIndex, int endIndex) {
-  const long long INF = numeric_limits<long long>::max(); // set INF to largest int
+string solve(vector<vector<graph_edge> > graph, long long startIndex, long long endIndex) {
+  const long long INF = numeric_limits<long long>::max(); // set INF to largest long long
   bool *visited = new bool[graph.size()]; // if node has been included in shortest path already
   long long *distances = new long long[graph.size()]; // distance of each node to source
   priority_queue<Node, vector<Node>, decltype(comp)> pq(comp); // min heap sorted by distance
-  int *parentNodes = new int[graph.size()]; // store index of parent of each node in the shortest path
+  long long *parentNodes = new long long[graph.size()]; // store index of parent of each node in the shortest path
 
   // initializations
   distances[startIndex] = 0;
   pq.push(Node(startIndex, 0, 0));
-  for (int i = 0; i < graph.size(); i++) {
+  for (long long i = 0; i < graph.size(); i++) {
     visited[i] = false;
     parentNodes[i] = -1;
     if (i != startIndex) {
@@ -56,9 +56,9 @@ string solve(vector<vector<graph_edge> > graph, int startIndex, int endIndex) {
   }
 
   vector<graph_edge> edges;
-  int clockTime; // the time when the current node is reached
-  int stoplightDist; // effective distance after accounting for waiting time
-  int newDistance; // the possibly shorter distance for adjacenet nodes
+  long long clockTime; // the time when the current node is reached
+  long long stoplightDist; // effective distance after accounting for waiting time
+  long long newDistance; // the possibly shorter distance for adjacenet nodes
 
   while (!pq.empty()) {
     Node currentNode = pq.top(); // node currently closest to sources
@@ -82,14 +82,16 @@ string solve(vector<vector<graph_edge> > graph, int startIndex, int endIndex) {
     for (auto &neighbor : edges) {
       // get the stopLight distance
       stoplightDist = getStoplightDist(clockTime, neighbor);
-
-      // we do not update the existing node in the priority queue
-      // instead, we insert a new copy of that node with a shorter distance
-      newDistance = distances[currentNode.index] + stoplightDist;
-      if (distances[neighbor.target] > newDistance) {
-        pq.push(Node(neighbor.target, newDistance, clockTime + stoplightDist));
-        distances[neighbor.target] = newDistance; // update distance
-        parentNodes[neighbor.target] = currentNode.index; // udpate parent node
+      // do something only when edge is crossable
+      if (stoplightDist != -1) {
+        // we do not update the existing node in the priority queue
+        // instead, we insert a new copy of that node with a shorter distance
+        newDistance = distances[currentNode.index] + stoplightDist;
+        if (distances[neighbor.target] > newDistance) {
+          pq.push(Node(neighbor.target, newDistance, clockTime + stoplightDist));
+          distances[neighbor.target] = newDistance; // update distance
+          parentNodes[neighbor.target] = currentNode.index; // udpate parent node
+        }
       }
     }
   }
@@ -99,18 +101,18 @@ string solve(vector<vector<graph_edge> > graph, int startIndex, int endIndex) {
   if (parentNodes[endIndex] == -1) {
     pathString = "Error: destination not reachable";
   } else {
-    vector<int> pathIndices; // path in reverse order
+    vector<long long> pathIndices; // path in reverse order
     pathIndices.push_back(endIndex);
-    int parent = parentNodes[endIndex];
+    long long parent = parentNodes[endIndex];
     while (parent != -1) {
       pathIndices.push_back(parent);
       parent = parentNodes[parent];
     }
 
-    int t = 0;
-    int startNode, endNode, startTime, endTime;
+    long long t = 0;
+    long long startNode, endNode, startTime, endTime;
     graph_edge edge;
-    for (int i = pathIndices.size() - 1; i > 0; i--) {
+    for (long long i = pathIndices.size() - 1; i > 0; i--) {
       startNode = pathIndices[i];
       endNode = pathIndices[i - 1];
       // get the edge between startNode and endNode
