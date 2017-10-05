8476139f017a7d15774928507e31ca05d0f0597e
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-05 12:14:26 -0400

    Successfully parsed graph for client

--- adversarial-shortest-path/client.cpp
+++ adversarial-shortest-path/client.cpp
@@ -3,16 +3,22 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <utility>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Client {
   SocketClient *socket;
+  map<string, int> encoder;
+  map<int, string> decoder;
+  pair<string, string> targetNodes;
 
   public:
     Client(string ip, int port, bool isTraverser);
-    json getGraph();
+    vector<vector<int> > getGraph();
 };
 
 Client::Client(string ip, int port, bool isTraverser) {
@@ -25,13 +31,54 @@ Client::Client(string ip, int port, bool isTraverser) {
   socket->sendJSON(playerInfo);
 }
 
-json Client::getGraph() {
-  return socket->receiveJSON(2000*1000);
+vector<vector<int> > Client::getGraph() {
+  json graphInfo = socket->receiveJSON(2000*1000);
+  targetNodes = make_pair(graphInfo["start_node"], graphInfo["end_node"]);
+  json graph = graphInfo["graph"];
+  string graph_string = graph.dump();
+  int numNodes = 0;
+  for (char c : graph_string) {
+    if (c == '[') numNodes++;
+  }
+  vector<vector<int> > adjacencyLists(numNodes);
+  bool inList = false;
+  int currentEncodedNode = -1;
+  bool inString = false;
+  string nodeName = "";
+  int curMaxIndex = 0;
+  for (char c : graph_string) {
+    if (c == '"') {
+      if (inString) {
+        // We are at the end of a nodeName
+        if (!encoder.count(nodeName)) {
+          // We haven't encoded this name yet
+          encoder[nodeName] = curMaxIndex;
+          decoder[curMaxIndex] = nodeName;
+          curMaxIndex++;
+        }
+        if (!inList) {
+          // This is a key
+          currentEncodedNode = encoder[nodeName];
+        } else {
+          // This is an adjacent node
+          adjacencyLists[currentEncodedNode].push_back(encoder[nodeName]);
+        }
+      } else {
+        // We are at the start of a nodeName
+        nodeName = "";
+      }
+      inString = !inString;
+    } else if (c == '[' || c == ']') {
+      inList = !inList;
+    } else if (inString) {
+      nodeName += c;
+    }
+  }
+  return adjacencyLists;
 }
 
 int main() {
   Client traverser("127.0.0.1", 8080, true);
   Client adversary("127.0.0.1", 8080, false);
-  json graph = traverser.getGraph();
-  cout << graph.dump(4) << endl;
+  vector<vector<int> > graph = traverser.getGraph();
 }
