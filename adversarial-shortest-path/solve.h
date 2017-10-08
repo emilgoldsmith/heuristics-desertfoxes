f2d8b1357c3f2240a295cc3c55a8f84d8eefdbe9
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-08 19:36:31 -0400

    Add visited check to minimax

--- adversarial-shortest-path/solve.h
+++ adversarial-shortest-path/solve.h
@@ -5,9 +5,10 @@
 #include "asp_game_state.h"
 
 #include <vector>
+#include <bitset>
 
-Move miniMaxAdversary(ASPGameState *state);
-Move miniMaxTraverser(ASPGameState *state);
+Move miniMaxAdversary(ASPGameState *state, std::bitset<1010> visited);
+Move miniMaxTraverser(ASPGameState *state, std::bitset<1010> visited);
 Move getTraverseMove(ASPGameState *state);
 Move getAdversaryMove(ASPGameState *state);
 
