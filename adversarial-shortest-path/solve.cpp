0dfbd5ec80e52aa9cb744a41125650b895441408
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-08 22:50:24 -0400

    Added sorting heuristic for ordering of minimax search

--- adversarial-shortest-path/solve.cpp
+++ adversarial-shortest-path/solve.cpp
@@ -46,6 +46,22 @@ Move miniMaxAdversary(ASPGameState *state, bitset<1010> visited, long double alp
   return bestMove;
 }
 
+void bubbleSort(ASPGameState *state, int currentNode) {
+  bool isSorted = false;
+  vector<int>& v = (*(state->graph))[currentNode];
+  int cnt = 0;
+  while (!isSorted) {
+    cnt++;
+    isSorted = true;
+    for (int i = 0; i < v.size() - 1; i++) {
+      if (state->distances[v[i]] > state->distances[v[i+1]]) {
+        swap(v[i], v[i+1]);
+        isSorted = false;
+      }
+    }
+  }
+}
+
 Move miniMaxTraverser(ASPGameState *state, bitset<1010> visited, long double alpha, long double beta) {
   vector<vector<int> > *graph = state->graph;
   int currentNode = state->currentNode;
@@ -62,6 +78,22 @@ Move miniMaxTraverser(ASPGameState *state, bitset<1010> visited, long double alp
     -1,
     state->INF
   };
+
+
+
+  // sort((*graph)[currentNode].begin(), (*graph)[currentNode].end(),
+  //     [state] (const int& a, const int& b) {
+  //   return state->distances[a] < state->distances[b];
+  // });
+
+  bubbleSort(state, currentNode); // For 1000 sorts this outperformed the above by a factor of 2 on the 300 node graph, so we keep it for now
+
+  int last = (*graph)[currentNode][0];
+  for (int neighbour: (*graph)[currentNode]) {
+    if (state->distances[last] > state->distances[neighbour]) {
+      cerr << "FOUL" << endl;
+    }
+  }
   for (int neighbour : (*graph)[currentNode]) {
     if (visited.test(neighbour)) {
       continue;
