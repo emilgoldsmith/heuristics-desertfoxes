f09f96e3aa061261b57c24d17b008abe5b6ddbdb
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-23 15:44:33 -0400

    Attempt at fixing main.cpp

--- evasion/main.cpp
+++ evasion/main.cpp
@@ -7,16 +7,20 @@ using namespace std;
 
 int main() {
   string ip = "127.0.0.1";
-  int port = 9001;
+  int port = 9000;
   EvasionClient client(ip, port);
 
   HunterMove hm;
   Position pm;
   while (true) {
     if (client.isHunter) {
-      hm = client.hunterMakeMove(solveHunterRandom);
-      client.receiveUpdate();
+      // The hunter's update is before the prey has moved
+      if (client.state->score > 0) {
+        // The first update is done in the constructor of client otherwise or by the last prey receive
+        client.receiveUpdate();
+      }
       pm = client.parsePreyMove();
+      hm = client.hunterMakeMove(solveHunterRandom);
       cout << "Parsed prey move: " << pm.x << ", " << pm.y << endl;
       client.state->makeMove(hm, pm);
     } else {
