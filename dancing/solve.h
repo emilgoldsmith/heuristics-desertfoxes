2ca0511108d4659efa8befd9683abd400686904a
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-28 12:39:14 -0400

    Connected game state and client and completed pairingsToPositions

--- dancing/solve.h
+++ dancing/solve.h
@@ -10,6 +10,8 @@
 Point computeCenter(std::vector<Point> points);
 Point computeCenterBruteforce(const std::vector<Point> &points);
 Point getFurthestPoint(const Point &center, const std::vector<Point> &points, bool getSecondFurthestPoint = false);
+std::vector<Pairing> getPairing(Client *client);
+SolutionSpec pairingsToPositions(Client *client, std::vector<Pairing> pairings);
 std::vector<Point> dummyPlaceStars(Client *client);
 ChoreographerMove dummyGetChoreographerMove(Client *client);
 
