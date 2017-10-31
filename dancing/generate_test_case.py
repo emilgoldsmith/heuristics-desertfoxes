7cdacd92d9a2116a896072b7aac16aa253c0a518
Guyu Fan <guyufan1996@gmail.com>
2017-10-31 14:33:11 -0400

    small format error, probably does not matter

--- dancing/generate_test_case.py
+++ dancing/generate_test_case.py
@@ -3,7 +3,7 @@ import sys, random
 def generate(board_size, num_dancers, num_colors):
   board = [[0 for i in range(board_size)] for j in range(board_size)]
   for c in range(num_colors):
-    print('Dancer positions for color {}'.format(c + 1))
+    print('Dancer positions for color: {}'.format(c + 1))
     for d in range(num_dancers):
       x = random.randint(0, board_size - 1)
       y = random.randint(0, board_size - 1)
