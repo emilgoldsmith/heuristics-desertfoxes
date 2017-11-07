91cab61c455a1cf90e4a3f53f9111ad9a41042c3
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-11-07 16:21:46 -0500

    Add hill climber and randomized dfs

--- compatibility/solver.cpp
+++ compatibility/solver.cpp
@@ -1,4 +1,5 @@
 #include "../random/random.h"
+#include "../timer/timer.h"
 
 #include <iostream>
 #include <algorithm>
@@ -14,42 +15,7 @@ struct Package {
 bool adjacent[25][45][25][45]; // this is automatically initialized to 0
 int numP, numV, numC;
 
-vector<Package> getSANeighbour(vector<Package> source) {
-  Random r;
-  // Determine the orders that we try finding a neighbor in
-  vector<int> packageCheckOrder(numV, 0);
-  for (int i = 0; i < numP; i++) {
-    packageCheckOrder[i] = i + 1;
-  }
-  shuffle(packageCheckOrder.begin(), packageCheckOrder.end(), r.generator);
-
-  vector<int> versionCheckOrder(numV, 0);
-  for (int i = 0; i < numV; i++) {
-    versionCheckOrder[i] = i + 1;
-  }
-
-  for (int p : packageCheckOrder) {
-    shuffle(versionCheckOrder.begin(), versionCheckOrder.end(), r.generator);
-    for (int v : versionCheckOrder) {
-      if (v == source[p - 1].v) continue;
-      bool validSwap = true;
-      for (Package pkg : source) {
-        if (pkg.p != p) {
-          validSwap = validSwap && adjacent[p][v][pkg.p][pkg.v];
-          if (!validSwap) break;
-        }
-      }
-      if (validSwap) {
-        source[p-1] = {p, v};
-        return source;
-      }
-    }
-  }
-  cerr << "Reached the end of getSANeighbour! Not intended!" << endl;
-  return source;
-}
-
-double getSACost(vector<Package> config) {
+double getCost(vector<Package> config) {
   int versionSum = 0;
   for (Package pkg : config) {
     versionSum += pkg.v;
@@ -57,9 +23,42 @@ double getSACost(vector<Package> config) {
   return versionSum;
 }
 
+vector<Package> hillClimb(vector<Package> source) {
+  Random r;
+  vector<int> packageCheckOrder(numP, 0);
+  for (int i = 0; i < numP; i++) {
+    packageCheckOrder[i] = i + 1;
+  }
+  while (true) {
+    // Determine the orders that we try finding a neighbor in
+    shuffle(packageCheckOrder.begin(), packageCheckOrder.end(), r.generator);
+
+    bool validSwap;
+    for (int i : packageCheckOrder) {
+      int p = source[i - 1].p;
+      for (int v = source[i - 1].v + 1; v <= numV; v++) {
+        validSwap = true;
+        for (Package pkg : source) {
+          if (pkg.p != p) {
+            validSwap = validSwap && adjacent[p][v][pkg.p][pkg.v];
+            if (!validSwap) break;
+          }
+        }
+        if (validSwap) {
+          source[i-1] = {p, v};
+          break;
+        }
+      }
+    }
+    if (!validSwap) break;
+  }
+  return source;
+}
+
+vector<int> packageCheckOrder;
 vector<Package> dfs(vector<Package> currentConfig) {
   if (currentConfig.size() == numP) return currentConfig;
-  int p = currentConfig.back().p + 1;
+  int p = packageCheckOrder[currentConfig.size()];
   for (int v = numV; v > 0; v--) {
     bool allValid = true;
     for (Package curPkg : currentConfig) {
@@ -86,38 +85,26 @@ int main() {
     adjacent[p1][v1][p2][v2] = adjacent[p2][v2][p1][v1] = true;
   }
 
-  vector<Package> bestConfig;
-  for (int v = numV; v > 0; v--) {
-    vector<Package> initialConfig = {{1, v}};
-    bestConfig = dfs(initialConfig);
-    if (bestConfig.size() > 0) break;
+  for (int i = 0; i < numP; i++) {
+    packageCheckOrder.push_back(i + 1);
   }
-
+  vector<Package> bestConfig;
+  int bestCost = 0;
   Random r;
-  vector<Package> oldConfig = bestConfig;
-  double oldCost = getSACost(oldConfig);
-  double bestCost = oldCost;
-  double T = 1000.0;
-  double T_min = 0.00001;
-  double alpha = 0.945;
-  while (T > T_min) {
-    for (int i = 0; i < 100; i++) {
-      vector<Package> newConfig = getSANeighbour(oldConfig);
-      double newCost = getSACost(newConfig);
-      double acceptanceProbability = exp((newCost - oldCost)/T); // We try to maximize instead of minimize
-      if (acceptanceProbability > r.randDouble(0, 1)) {
-        oldConfig = newConfig;
-        oldCost = newCost;
-        if (oldCost > bestCost) {
-          bestCost = oldCost;
-          bestConfig = oldConfig;
-        }
-      }
+  for (int i = 0; i < 100; i++) {
+    shuffle(packageCheckOrder.begin(), packageCheckOrder.end(), r.generator);
+    vector<Package> initialConfig;
+    vector<Package> candidateConfig = hillClimb(dfs(initialConfig));
+    int candidateCost = getCost(candidateConfig);
+    if (candidateCost > bestCost) {
+      bestCost = candidateCost;
+      bestConfig = candidateConfig;
     }
-    T *= alpha;
   }
 
   // Output answer
+  sort(bestConfig.begin(), bestConfig.end(),
+      [](const Package &a, const Package &b) -> bool {return a.p < b.p;});
   cout << 1 << endl;
   for (int i = 1; i <= numP; i++) {
     if (bestConfig[i-1].p != i) {
